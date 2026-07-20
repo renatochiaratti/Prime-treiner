1732b03db1c19ab18a18df4d2574014d98d5d040
renatochiaratti <renato_0301@hotmail.com>
2026-07-20 09:24:24 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,6 @@
-const CACHE = "prime-trainer-v1";
-const SHELL = ["/", "/manifest.json"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).catch(() => {}));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request)
-      .then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
-        return res;
-      })
-      .catch(() => caches.match(event.request).then((res) => res || caches.match("/")))
-  );
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", async () => {
+  await self.registration.unregister();
+  const clientsArr = await self.clients.matchAll({ type: "window" });
+  clientsArr.forEach((client) => client.navigate(client.url));
 });
